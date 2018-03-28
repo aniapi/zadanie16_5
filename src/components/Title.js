4e683a89ea3d28cfbf4aa0c5ad896f4e16a74091
Anna Piwnik <a.piwnik@o2.pl>
2018-03-28 12:51:58 +0200

    Start task

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Title extends React.Component {
+	render() {
+	    return (
+	        <div className={Title}>
+	            <h2>To jest tytuł.</h2>
+	        </div>    
+	    );
+	}
+}
+
+export default Title;
